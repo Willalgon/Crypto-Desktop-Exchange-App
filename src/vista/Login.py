a59da6d8b4923044572c8bf5b94e74c34615176e
Guillermo Álvarez González <guillermoalvarezgonzalez06@gmail.com>
2026-04-22 15:39:23 +0200

    Save local changes before pulling

--- src/vista/Login.py
+++ src/vista/Login.py
@@ -1,33 +1,38 @@
-import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import uic
-from src.Modelo.vo.LoginVo import LoginVo
+from src.modelo.vo.LoginVo import LoginVo
 
-Form, Window = uic.loadUiType("/Users/samuelllamas/Documents/data/IngSoftware/github/PROYECTO_SOFTWARE/src/vista/ui/MainWindow.ui")
+# Cargar la interfaz generada desde el archivo .ui
+Form, Window = uic.loadUiType("./src/vista/Ui/Login.ui")
 
 class MiVentana(QMainWindow, Form):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-       
-        self.Aceptar.clicked.connect(self.on_button_click)
+        self.setupUi(self)  # Inicializa los widgets
+        self.controlador=None
+        # Conectar el botón a la función
+        self.Boton.clicked.connect(self.on_button_click)
+
 
     def on_button_click(self):
+        print("Botón presionado")
+        texto_nombre=self.textoNombre.text() #Obtener el texto del campo nombre
+        texto_contrasena=self.ContrasenaEdit.text()
+        self.controlador.comprobarLogin(texto_nombre,texto_contrasena)
+    def lanzaraviso(self):
+        QMessageBox.information(self,"Info", "Login incorrecto")
+    @property
+    def controlador(self):
+        return self.controlador
+    @controlador.setter
+    def controlador(self,ref_controlador):
+        self.controlador=ref_controlador
 
-        nombre = self.lineEdit.text()
-        password = self.lineEdit_2.text()
         
-        print("Nombre capturado:", nombre)
-        print("Password capturada:", password)
-
-        
-        login = LoginVo(nombre, password)
-        
-        print("Objeto VO creado:", login)
-        
-       
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QApplication([])
     ventana = MiVentana()
     ventana.show()
-    sys.exit(app.exec_())
+    app.exec_()
+
+
