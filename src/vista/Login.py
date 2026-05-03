8d4a39f4a12023aab76b07e0ff17b4819298623c
Guillermo Álvarez González <guillermoalvarezgonzalez06@gmail.com>
2026-05-03 19:54:05 +0200

    Updates

--- src/vista/Login.py
+++ src/vista/Login.py
@@ -1,20 +1,19 @@
 from PyQt5.QtWidgets import QMainWindow, QMessageBox
 from PyQt5 import uic
 
-# Asegúrate de que el archivo se llame exactamente así en la carpeta
 Form, Window = uic.loadUiType("./src/vista/ui/MainWindow.ui")
 
 class MiVentana(QMainWindow, Form):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)  
+        self.setupUi(self)
         self._controlador = None # Variable interna con guion bajo
-        
+
         self.Aceptar.clicked.connect(self.on_button_click)
         self.btn_ir_registro.clicked.connect(self.on_registro_click)
 
     def on_button_click(self):
-        texto_nombre = self.lineEdit.text() 
+        texto_nombre = self.lineEdit.text()
         texto_contrasena = self.lineEdit_2.text()
         if self._controlador:
             self._controlador.comprobarLogin(texto_nombre, texto_contrasena)
@@ -29,7 +28,7 @@ class MiVentana(QMainWindow, Form):
     @property
     def controlador(self):
         return self._controlador
-    
+
     @controlador.setter
     def controlador(self, ref_controlador):
         self._controlador = ref_controlador
